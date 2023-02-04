608fb76a90e46073751c605012296323aa5d6636
Raamishshah010 <raamishshah010@gmail.com>
2023-02-05 03:01:34 +0500

    Working on Redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router,Routes,Route  } from 'react-router-dom';
-import Login from './components/Login'
-import Header from './components/Header';
-import Home from './components/Home';
+  import { BrowserRouter as Router,Routes,Route  } from 'react-router-dom';
+  import Login from './components/Login'
+  import Header from './components/Header';
+  import Home from './components/Home';
 
-import './App.css';
+  import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
-  );
-}
+  function App() {
+    return (
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    );
+  }
 
-export default App;
+  export default App;
